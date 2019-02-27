36eed06820820d8d7c9553550ffb20f18017525c
cmsusergit <cmsusergit@gmail.com>
2019-02-27 16:17:19 +0530

    report done

--- common/models/attndance-info.js
+++ common/models/attndance-info.js
@@ -1,5 +1,54 @@
 'use strict';
+const _=require('lodash')
 
 module.exports = function(Attndanceinfo) {
-
+Attndanceinfo.getAttendanceReport=function(input,cb){
+   let prList1=[]
+    Attndanceinfo.find({
+      where:{fstudenrollId:input.enroll}
+    }).then(temp=>{
+      temp.map(ob=>{
+          const pr=new Promise(function(resolve, reject) {
+            Attndanceinfo.app.models.DdClassSchedule.findOne(
+            {
+              where:{csId:ob.fclassscheduleid,ftimetableid:input.ttId}
+            }).then(r1=>{
+                if(r1)
+                  resolve({present:ob.attPresent,cshedule:r1.csDateConducted})
+                else {
+                  resolve(null)
+                }
+            })
+            .catch(e1=>{
+              console.log('****',e1);
+            })
+          });
+          prList1.push(pr)
+      });
+      Promise.all(prList1).then(rr=>{
+        _.remove(rr,ob=>{
+          return !ob;
+        })
+        cb(null,rr);
+      })
+    })
+    .catch(error=>{
+      console.log('****',error);
+      cb(error,null)
+    })
+  }
+  Attndanceinfo.remoteMethod('getAttendanceReport',{
+    accepts:[{
+      arg:'dt',
+      type:'Object'
+    }],
+    http:{
+      path:'/getAttendanceList',
+      verb:'post'
+    },
+    returns:{
+      arg:'attndList',
+      type:'array'
+    }
+    })
 };
--- common/models/timetable-record-info.js
+++ common/models/timetable-record-info.js
@@ -1,44 +1,80 @@
 'use strict';
 module.exports = function(Timetablerecordinfo) {
 
-  Timetablerecordinfo.getschedulelist=function(id,cb){
-      let prList1=[]
-       Timetablerecordinfo.findOne(
-       {
-         where:{ttId:id},
-         include:['ddClassSchedules']
-      })
-      .then(temp=>{
+  Timetablerecordinfo.getAttendanceList=function(id,cb){
+    Timetablerecordinfo.findOne({
+        where:{
+          ttId:id
+        },
+        fields:{
+          ttId:1
+        },
+        include:{
+          relation:"ddClassSchedules",
+          scope:{
 
-
-        temp.ddClassSchedules().map(ob=>{
-          let tt=Timetablerecordinfo.app.models.DdClassSchedule.getAttendanceList(ob);
-          prList1.push(tt);
-        });
-        Promise.all(prList1).then(rr=>{
-          console.log('----',rr);
-          cb(null,rr);
-        })
-      })
-      .catch(error=>{
-        console.log('****',error);
-        cb(error,null);
-      });
-    }
-Timetablerecordinfo.remoteMethod('getschedulelist',{
-      accepts:[{
-        arg:'id',
-        type:'number'
-      }],
-      http:{
-        path:'/getschedulelist/:id',
-        verb:'get'
-      },
-
-      returns:{
-        arg:'attndList',
-        type:'Object'
-      }
-
-  })
+              include:["attndanceInfos"]
+            }
+        }
+    }).then(rr=>{
+      cb(null,rr)
+    })
+    .catch(error=>{
+      cb(error,null)
+    })
+  }
+  Timetablerecordinfo.remoteMethod('getAttendanceList',{
+        accepts:[{
+          arg:'id',
+          type:'number'
+        }],
+        http:{
+          path:'/getAttendanceList/:id',
+          verb:'get'
+        },
+        returns:{
+         arg:'attndList',
+         type:'Object'
+       }
+  });
+//   Timetablerecordinfo.getschedulelist=function(id,cb){
+//       let prList1=[]
+//
+//        Timetablerecordinfo.findOne(
+//        {
+//          where:{ttId:id},
+//          include:['ddClassSchedules']
+//       })
+//       .then(temp=>{
+//
+//
+//         temp.ddClassSchedules().map(ob=>{
+//           let tt=Timetablerecordinfo.app.models.DdClassSchedule.getAttendanceList(ob);
+//           prList1.push(tt);
+//         });
+//         Promise.all(prList1).then(rr=>{
+//           cb(null,rr);
+//         })
+//       })
+//       .catch(error=>{
+//         console.log('****',error);
+//         cb(error,null);
+//       });
+//     }
+// Timetablerecordinfo.remoteMethod('getschedulelist',{
+//       accepts:[{
+//         arg:'id',
+//         type:'number'
+//       }],
+//       http:{
+//         path:'/getschedulelist/:id',
+//         verb:'get'
+//       },
+//
+//       returns:{
+//         arg:'attndList',
+//         type:'Object'
+//       }
+//
+//   })
 };
