c54518a4101c5100e7e58d7547d25041b8094677
cmsusergit <cmsusergit@gmail.com>
2019-04-20 16:15:58 +0530

    Recover With DeptId TTrecord

--- common/models/timetable-record-info.js
+++ common/models/timetable-record-info.js
@@ -1,5 +1,80 @@
 'use strict';
-
 module.exports = function(Timetablerecordinfo) {
 
+  Timetablerecordinfo.getAttendanceList=function(id,cb){
+    Timetablerecordinfo.findOne({
+        where:{
+          ttId:id
+        },
+        fields:{
+          ttId:1
+        },
+        include:{
+          relation:"ddClassSchedules",
+          scope:{
+
+              include:["attndanceInfos"]
+            }
+        }
+    }).then(rr=>{
+      cb(null,rr)
+    })
+    .catch(error=>{
+      cb(error,null)
+    })
+  }
+  Timetablerecordinfo.remoteMethod('getAttendanceList',{
+        accepts:[{
+          arg:'id',
+          type:'number'
+        }],
+        http:{
+          path:'/getAttendanceList/:id',
+          verb:'get'
+        },
+        returns:{
+         arg:'attndList',
+         type:'Object'
+       }
+  });
+//   Timetablerecordinfo.getschedulelist=function(id,cb){
+//       let prList1=[]
+//
+//        Timetablerecordinfo.findOne(
+//        {
+//          where:{ttId:id},
+//          include:['ddClassSchedules']
+//       })
+//       .then(temp=>{
+//
+//
+//         temp.ddClassSchedules().map(ob=>{
+//           let tt=Timetablerecordinfo.app.models.DdClassSchedule.getAttendanceList(ob);
+//           prList1.push(tt);
+//         });
+//         Promise.all(prList1).then(rr=>{
+//           cb(null,rr);
+//         })
+//       })
+//       .catch(error=>{
+//         console.log('****',error);
+//         cb(error,null);
+//       });
+//     }
+// Timetablerecordinfo.remoteMethod('getschedulelist',{
+//       accepts:[{
+//         arg:'id',
+//         type:'number'
+//       }],
+//       http:{
+//         path:'/getschedulelist/:id',
+//         verb:'get'
+//       },
+//
+//       returns:{
+//         arg:'attndList',
+//         type:'Object'
+//       }
+//
+//   })
 };
